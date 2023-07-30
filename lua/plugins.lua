5ff93715246a8af054321acdbd8e1bc8761c2a42
matvey <tingaev.m.s@gmail.com>
2023-07-30 14:51:25 +0300

    Added light-speed for navigation

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -38,6 +38,7 @@ return require('packer').startup(function(use)
   use {"nvim-lualine/lualine.nvim"}
   use {"nvim-telescope/telescope.nvim"}
   use {"nvim-lua/plenary.nvim"}
+  use {"ggandor/lightspeed.nvim"}
 
   -- Automatically set up your configuration after cloning packer.nvim
   -- Put this at the end after all plugins
