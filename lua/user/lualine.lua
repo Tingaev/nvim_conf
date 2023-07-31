b924576a32908f5c83809389aa5d3b4eead4a2a2
matvey <tingaev.m.s@gmail.com>
2023-07-31 21:11:25 +0300

    Disable bufferline and edited lualine include

--- lua/user/lualine.lua
+++ lua/user/lualine.lua
@@ -1,10 +1,7 @@
 local status_ok, lualine = pcall(require, "lualine")
+
 if not status_ok then
 	return
-end
-
-local hide_in_width = function()
-	return vim.fn.winwidth(0) > 80
 end
 
 local diagnostics = {
@@ -17,24 +14,11 @@ local diagnostics = {
 	always_visible = true,
 }
 
-local diff = {
-	"diff",
-	colored = false,
-	symbols = { added = " ", modified = " ", removed = " " }, -- changes diff symbols
-  cond = hide_in_width
-}
-
 local mode = {
 	"mode",
 	fmt = function(str)
 		return "-- " .. str .. " --"
 	end,
-}
-
-local filetype = {
-	"filetype",
-	icons_enabled = false,
-	icon = nil,
 }
 
 local branch = {
@@ -49,10 +33,6 @@ local location = {
 }
 
 
-local spaces = function()
-	return "spaces: " .. vim.api.nvim_buf_get_option(0, "shiftwidth")
-end
-
 lualine.setup({
 	options = {
 		icons_enabled = true,
@@ -63,12 +43,10 @@ lualine.setup({
 		always_divide_middle = true,
 	},
 	sections = {
-		lualine_a = { branch, diagnostics },
+	--	lualine_a = { branch, diagnostics },
+		lualine_a = { branch },
 		lualine_b = { mode },
 		lualine_c = {},
-		-- lualine_x = { "encoding", "fileformat", "filetype" },
-		lualine_x = { diff, spaces, "encoding", filetype },
---		lualine_y = { location },
 	},
 	inactive_sections = {
 		lualine_a = {},
