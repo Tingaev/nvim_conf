342f9bb115c468237c48b3028de996ac57829d65
Matvey Tingaev <matvey@MacBook-Pro-Matvey.local>
2023-10-25 00:04:47 +0300

    Fix nvim tree loading

--- lua/matveyder/nvim-tree.lua
+++ lua/matveyder/nvim-tree.lua
@@ -3,12 +3,7 @@ if not status_ok then
   return
 end
 
-local config_status_ok, nvim_tree_config = pcall(require, "nvim-tree.config")
-if not config_status_ok then
-  return
-end
-
-require("nvim-tree").setup {
+require("nvim-tree").setup{
     view = {
         adaptive_size = true,
         number = true,
